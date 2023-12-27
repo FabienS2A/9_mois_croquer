146d828d0e76bb9ff433d45f091242323a59469b
pierre giannucci <pierregia13@gmail.com>
2023-12-27 10:40:30 +0100

    'nouveau fichier app2 et fichier config'

---
+++ app2.py
@@ -0,0 +1,59 @@
+import meilisearch
+from sqlalchemy import create_engine, MetaData, Table
+from flask import Flask, jsonify, request
+
+client = meilisearch.Client('http://localhost:7700')
+
+db_url = "mysql+pymysql://root:root@127.0.0.1:3306/9mois"
+engine = create_engine(db_url)
+metadata = MetaData()
+
+def meilei_add(table, list_col):
+    '''table is a string with the name of the table to add, list_col is a list of string representing selected columns to index'''
+    table_obj = Table(table, metadata, autoload_with=engine)
+    with engine.connect() as connection :
+        result_set = connection.execute(table_obj.select()).fetchall()
+        #Fetching all the table is convenient in developpement but inefficient, should be fixed later
+    documents = []
+    for row in result_set:
+        document = {column.name: getattr(row, column.name) for column in table_obj.columns if column.name in list_col}
+        documents.append(document)
+    #workaround for the food table
+    if table == 'food':
+        client.index(table).update(primary_key='code')
+    client.index(table).add_documents(documents) 
+
+    #list of retrieved columns
+list_recipes = ['id', 'name', 'budget', 'difficulty', 'food']
+list_articles = ['id', 'title', 'content' ]
+list_food = [ 'code', 'name']
+list_questions = [ 'id', 'question', 'answer', 'url_article']
+#adding table to meili
+meilei_add('recipes', list_recipes)
+meilei_add('articles', list_articles)
+meilei_add('food', list_food)
+meilei_add('questions', list_questions)
+
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def moteur_de_recherche():
+    search_query = request.args.get('q', '')  # Obtenir le paramètre 'q' de l'URL, en utilisant une chaîne vide comme valeur par défaut
+
+    if search_query:
+        resultats_recherche = client.multi_search(
+            [
+                {'indexUid': 'food', 'q': search_query, 'limit': 10},
+                {'indexUid': 'recipes', 'q': search_query, 'limit': 10},
+                {'indexUid': 'articles', 'q': search_query, 'limit': 10},
+                {'indexUid': 'questions', 'q': search_query, 'limit': 10}
+            ]
+        )
+        return jsonify(resultats_recherche)
+    else:
+        return jsonify({"erreur": "Aucune requête de recherche fournie"}), 400
+
+if __name__ == "__main__":
+    app.run(debug=True)
